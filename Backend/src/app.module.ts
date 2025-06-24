a9984586f95e27a1619d25a45d701567f72f339e
Petrus Handika <petrushandikasinaga@gmail.com>
2025-06-24 12:29:36 +0700

    chore: implement monolith architecture

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ProductsModule } from './modules/products/products.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, ProductsModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
