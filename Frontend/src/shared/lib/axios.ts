8f49ad7810320d748ef6e45d3be7456030aa9563
Petrus Handika <petrushandikasinaga@gmail.com>
2025-11-19 07:53:20 +0700

    refactor: unify string quotes and update API base URL
    Changed string quotes from double to single for consistency in auth API and axios configuration files. Updated the base URL environment variable from NEXT_PUBLIC_API_BASE_URL to NEXT_PUBLIC_API_URL for improved clarity.

--- Frontend/src/shared/lib/axios.ts
+++ Frontend/src/shared/lib/axios.ts
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import axios from "axios";
-import { useAuthStore } from "@/features/auth/store/auth.store";
+import axios from 'axios';
+import { useAuthStore } from '@/features/auth/store/auth.store';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use(
@@ -12,7 +12,7 @@ api.interceptors.request.use(
     const token = useAuthStore.getState().token;
 
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
 
     return config;
